4265e0b0d9e2f03e52a48ab4408e197496100cb3
1334155539 <1334155539@qq.com>
2021-02-05 11:50:28 +0800

    自定义注解的反射实现

--- spring_demo/src/main/java/org/annotation/demo/Calcul.java
+++ spring_demo/src/main/java/org/annotation/demo/Calcul.java
@@ -8,10 +8,10 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class Calcul {
-    @MyAnnotation(max = 50, min = 0)
-    int ma = 0;
-    @MyAnnotation(max = 50, min = 0)
-    int mb = 0;
+    @MyAnnotation(max = 25, min = 0)
+    public int ma = 0;
+    @MyAnnotation(max = 10, min = 0)
+    public int mb = 0;
 
     public Calcul(int ma, int mb) {
         this.ma = ma;
@@ -33,25 +33,29 @@ public class Calcul {
     }
 
     public void testManno() throws IllegalArgumentException, IllegalAccessException {
-        Class clazz = Calcul.class;
+//        Class clazz = Calcul.class;
+        Class clazz = this.getClass();
 
         for(Field field : clazz.getDeclaredFields()) {
             if (field.isAnnotationPresent(MyAnnotation.class)) {
                 MyAnnotation reange =  field.getAnnotation(MyAnnotation.class);
-
+                field.setAccessible(true);
                 System.out.println("Fields名： " + field.getName() + " 已经被 注释标记 ，标记名： " + field.getAnnotation(MyAnnotation.class) );
-                if ((Integer)field.getInt("") > reange.max() || (Integer)field.getInt("") < reange.min()) {
+                System.out.println("--------------------");
+                System.out.println("--------------------" + field.get(this));
+                if ((Integer)field.get(this) > reange.max() || (Integer)field.get(this) < reange.min()) {
+                    System.out.println("传入值大于注解最大值或者小于注解最小值， 准备抛出异常");
                     throw new IllegalArgumentException();
                 } else {
-                    System.out.println("field.getInt(Integer.class) " +  field.getInt(Integer.class));
+                    System.out.println("未检出异常，输出log");
                 }
             }
         }
 
-        for (Method method: clazz.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(MyAnnotation.class)) {
-                System.out.println("方法名： " + method + " 已经被 注释标记 ，标记名： " + method.getAnnotation(MyAnnotation.class) );
-            }
-        }
+//        for (Method method: clazz.getDeclaredMethods()) {
+//            if (method.isAnnotationPresent(MyAnnotation.class)) {
+//                System.out.println("方法名： " + method + " 已经被 注释标记 ，标记名： " + method.getAnnotation(MyAnnotation.class) );
+//            }
+//        }
     }
 }
--- spring_demo/src/main/java/org/annotation/demo/MyAnnotationTest.java
+++ spring_demo/src/main/java/org/annotation/demo/MyAnnotationTest.java
@@ -9,13 +9,21 @@ import java.lang.annotation.Documented;
 
 public class MyAnnotationTest {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) {
 //        Calcul calcul = new Calcul();
-        Calcul calcul = new Calcul(1 , 25);
+        Calcul calcul = new Calcul(15 , 15);
 
         calcul.add();
 
-        calcul.testManno();
+        try {
+            calcul.testManno();
+        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+            System.out.println("抛出异常" +  e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("抛出异常" +  e.getMessage());
+//            e.printStackTrace();
+        }
 
         return;
     }
